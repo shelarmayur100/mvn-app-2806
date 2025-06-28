1239f78baa2f46ed61dcc847d77d5e606cfdccd6
mayur <shelarmayur100@gmail.com>
2025-06-28 21:53:04 +0530

    Initial commit

---
+++ src/main/java/com/dhoni/io/App.java
@@ -0,0 +1,13 @@
+package com.dhoni.io;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
